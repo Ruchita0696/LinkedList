f62cf8e05105670d7c5ca5bb561090ff53ca2f4f
ruchi06 <ruchi06@gmail.com>
2022-12-08 18:04:35 +0530

    Create Linked List

---
+++ LinkedList/LinkedList/LinkedList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class LinkedList
+    {
+        internal Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+
+            }
+            Console.WriteLine($"{node.data} is Inserted in Linked List\n");
+        }
+        public void Display()
+        {
+            int i = 1;
+            Node temp = head;
+
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is Empty");
+
+            }
+            while (temp != null)
+            {
+                Console.WriteLine($"Element {i} in Linked List is : " + temp.data);
+                temp = temp.next;
+                i++;
+            }
+        }
+        
+    }
+}
---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+
+        }
+    }
+}
